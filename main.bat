3624bd53d6f9a9d6f2708109c0dfe36b45847b15
Doronin Stanislav <mugisbrows@gmail.com>
2025-06-06 15:48:44 +0300

    test2

--- main.bat
+++ main.bat
@@ -3,3 +3,7 @@ rem This file is generated from main.pbat, all edits will be lost
 set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\msys64\ucrt64\bin;%PATH%
 where python
 python -m pip install mugideploy
+where mugideploy
+pdftoppm.exe -v
+python D:\dev\poppler-dist\assert-version.py 25.05.0 pdftoppm -v
+mugideploy collect --name poppler --version 25.05.0 --bin pdfattach.exe pdfdetach.exe pdffonts.exe pdfimages.exe pdfinfo.exe pdfseparate.exe pdfsig.exe pdftocairo.exe pdftohtml.exe pdftoppm.exe pdftops.exe pdftotext.exe pdfunite.exe --unix-dirs --data C:\msys64\ucrt64\share\poppler --zip
